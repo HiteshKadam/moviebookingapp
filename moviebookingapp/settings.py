86c198ac0ef890a948decffe4eb3afc42672248e
HiteshKadam <hdkadam2000@gmail.com>
2023-06-25 18:47:40 +0530

    rollback

--- moviebookingapp/settings.py
+++ moviebookingapp/settings.py
@@ -14,11 +14,6 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'movie-booking-ui/build/static'),
-]
-
-STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
